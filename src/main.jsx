a20b3a1279e31791969c0b2fd4efea0de05b0c29
Prakhar Pratap Singh <142224254+prakharpratapsingh21@users.noreply.github.com>
2025-10-09 16:29:02 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,6 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 // import Context from './utils/Context.jsx'
 import {Provider} from 'react-redux';
-import {store} from './Store/Store.jsx'
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//     <Context>
-//         <BrowserRouter>
-//             <App />
-//         </BrowserRouter>
-//     </Context>
-// )
-
 
 //---------------When working on Redux-------------------------
 ReactDOM.createRoot(document.getElementById('root')).render(

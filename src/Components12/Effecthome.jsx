f5878f4f87ae0c79c2a1fe2720bf1b10a624689b
Prakhar Pratap Singh <142224254+prakharpratapsingh21@users.noreply.github.com>
2025-10-09 16:23:27 +0530

    Delete src/Components12 directory

--- src/Components12/Effecthome.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Effecthome() {
-  return (
-    <div>Effecthome</div>
-  )
-}
-
-export default Effecthome
--- src/Components12/Nisha.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Nisha = () => {
-  return (
-    <div>Nisha</div>
-  )
-}
-
-export default Nisha
